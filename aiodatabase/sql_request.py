b19973f10e0ef64068a3e6ef0343982f232bafab
Sam <130869088+samdevelop3r@users.noreply.github.com>
2023-05-20 17:37:27 +0300

    Update sql_request.py

--- aiodatabase/sql_request.py
+++ aiodatabase/sql_request.py
@@ -1,20 +1,18 @@
 import asyncio
 import aiomysql
-import ujson
+
+from .configs.models import ConvertedUrl
 
 
 class SQLRequests:
-    with open("dbconfig.json", "r", encoding="utf-8") as _file:
-        _database_data = ujson.load(_file)
-
     @classmethod
-    async def send(cls, query: str) -> None | tuple:
+    async def send(cls, converted_url: ConvertedUrl, query: str) -> None | tuple:
         pool = await aiomysql.create_pool(
-            host=cls._database_data["hostname"],
-            port=cls._database_data["port"],
-            user=cls._database_data["user"],
-            password=cls._database_data["password"],
-            db=cls._database_data["db"], loop=asyncio.get_event_loop()
+            host=converted_url.hostname,
+            port=converted_url.port,
+            user=converted_url.user,
+            password=converted_url.password,
+            db=converted_url.db, loop=asyncio.get_event_loop()
         )
         async with pool.acquire() as conn:
             async with conn.cursor() as cur:
