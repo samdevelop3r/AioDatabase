5830859ff80404058834eaf08c5047e864464fcf
Sam <130869088+samdevelop3r@users.noreply.github.com>
2023-05-20 17:37:04 +0300

    Update database.py

--- aiodatabase/database.py
+++ aiodatabase/database.py
@@ -1,5 +1,6 @@
 from .table import Table
 from .sql_request import SQLRequests
+from .configs.converters import Url
 
 
 class AioDatabase:
@@ -10,25 +11,26 @@ class AioDatabase:
         "bool": " BOOLEAN "
     }
 
-    @classmethod
-    def _table_parameter_converter(cls, parameter: str, is_key: bool = False) -> str:
-        sorted_parameter = parameter.split(" ")[0] + cls._data_types[parameter.split(" ")[1]]
+    def __init__(self, url: str):
+        self._converted_url = Url.convert(url)
+
+    def _table_parameter_converter(self, parameter: str, is_key: bool = False) -> str:
+        sorted_parameter = parameter.split(" ")[0] + self._data_types[parameter.split(" ")[1]]
         if is_key is True:
             sorted_parameter += " PRIMARY KEY AUTOINCREMENT"
             return sorted_parameter
         return sorted_parameter
 
-    @classmethod
-    async def create_table(cls, name: str, primary_key: str | int = None, **kwargs) -> None:
+    async def create_table(self, name: str, primary_key: str | int = None, **kwargs) -> None:
         request_query = f"CREATE TABLE IF NOT EXISTS {name} ("
         if primary_key:
-            request_query += cls._table_parameter_converter(parameter=primary_key, is_key=True) + ", "
+            request_query += self._table_parameter_converter(parameter=primary_key, is_key=True) + ", "
         for key, value in kwargs.items():
-            request_query += cls._table_parameter_converter(parameter=f"{key} {value}") + ", "
+            request_query += self._table_parameter_converter(parameter=f"{key} {value}") + ", "
         request_query = request_query[:-2]
         request_query += ")"
-        await cls._requests.send(request_query)
+        await self._requests.send(converted_url=self._converted_url, query=request_query)
 
-    @classmethod
-    def table(cls, name: str):
-        return Table(name=name, requests=cls._requests)
+    def table(self, name: str):
+        return Table(name=name, requests=self._requests, converted_url=self._converted_url)
+    
